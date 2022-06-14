f9f532a10badfee41b65b8494bb8ae65a513cd0a
Davi Ferreira <daviferreirart@gmail.com>
2022-06-14 10:33:47 -0300

    checking error on user service

--- src/services/userServices/userService.ts
+++ src/services/userServices/userService.ts
@@ -2,10 +2,18 @@ import dbServices from "../database/databaseServices";
 
 export default class UserServices {
   public static async CreateNewUser(full_name: string) {
-    return dbServices.CreateNewUser(full_name);
+    const user = dbServices.CreateNewUser(full_name);
+    if (user != undefined) {
+      return user;
+    }
+    return undefined;
   }
 
   public static async UpdateSubscription(user_id: number, status: string) {
-    return dbServices.UpdateUserSubscriptionStatus(user_id, status);
+    const updatedUser = dbServices.UpdateUserSubscriptionStatus(user_id, status)
+    if (updatedUser != null){
+      return updatedUser
+    }
+    return null || undefined;
   }
 }
